c1c4dcfe7b16c54da19f0092d5b6deac2f73a7c2
Rahma Dini Daulay <99544032+yldin@users.noreply.github.com>
2022-06-04 14:37:08 +0700

    Create users.js

---
+++ Backend-API/Product-Item/routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+const res = require('express/lib/response');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send(process.env.APP_NAME);
+});
+
+router.get('/me', (req, res, next) => {
+  res.send('Hi this eurica!');
+});
+module.exports = router;
